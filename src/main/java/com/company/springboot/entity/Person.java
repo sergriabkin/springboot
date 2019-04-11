405d5c9fa3961b4406b98181ea535155cb7569b0
Serg <1992serg@gmail.com>
2019-04-11 23:10:06 +0300

    [hw1] persons with phones

--- src/main/java/com/company/springboot/entity/Person.java
+++ src/main/java/com/company/springboot/entity/Person.java
@@ -1,13 +1,8 @@
 package com.company.springboot.entity;
 
-import lombok.Data;
+import javax.persistence.*;
+import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
-@Data
 @Entity
 public class Person {
 
@@ -28,4 +23,40 @@ public class Person {
 
     @Column
     private Integer age;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "person")
+    private List<Phone> phones;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
--- src/main/java/com/company/springboot/entity/Phone.java
+++ src/main/java/com/company/springboot/entity/Phone.java
@@ -1,4 +1,54 @@
 package com.company.springboot.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "PHONES")
 public class Phone {
+
+    public Phone() {
+    }
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String number;
+
+    @ManyToOne
+    @JoinColumn(name = "PERSON_ID")
+    private Person person;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "id=" + id +
+                ", number='" + number + '\'' +
+                '}';
+    }
 }
